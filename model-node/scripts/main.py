b93cb2c6df2d3479a471953df2d9b774f26ccdbf
Haonan Yuan (袁昊男) <53254926+haonan-yuan@users.noreply.github.com>
2025-07-24 13:53:50 +0800

    Add files via upload

--- model-node/scripts/main.py
+++ model-node/scripts/main.py
@@ -174,14 +174,11 @@ def train_model(
                 sp_adj5 = sp_adj5.cuda()
 
     best = 1e9
-    firstbest = 0
 
     for epoch in range(args.nb_epochs):
         np.random.seed(seed)
         torch.manual_seed(seed)
         torch.cuda.manual_seed(seed)
-        loss = 0
-        regloss = 0
         model.train()
         optimiser.zero_grad()
         features_list = [features1, features2, features3, features4]
@@ -200,7 +197,6 @@ def train_model(
         print("Loss:[{:.4f}]".format(loss.item()))
         wandb.log({"pretrain_loss": loss.item()})
         if loss < best:
-            firstbest = 1
             best = loss
             best_t = epoch
             cnt_wait = 0
@@ -256,9 +252,6 @@ def train_model(
     model.load_state_dict(torch.load(args.save_name))
     embeds, _ = model.embed(features, sp_adj if sparse else adj, sparse, None, LP)
     downstreamlrlist = [downstreamlr]
-    acclist = torch.FloatTensor(
-        100,
-    ).cuda()
     xent = nn.CrossEntropyLoss()
     config = wandb.config
     data_path = config.data_path
@@ -275,9 +268,6 @@ def train_model(
             tot = torch.zeros(1)
             tot = tot.cuda()
             accs = []
-            cnt_wait = 0
-            best = 1e9
-            best_t = 0
             print("shotnum", shotnum)
 
             for i in tqdm(range(args.prompt_times)):
@@ -322,11 +312,7 @@ def train_model(
                 )
                 opt = torch.optim.Adam([{"params": log.parameters()}], lr=downstreamlr)
                 log = log.cuda()
-                best = 1e9
-                pat_steps = 0
-                best_acc = torch.zeros(1)
-                best_acc = best_acc.cuda()
-                best_acc = 0
+                best_loss = float('inf')
                 cnt_wait = 0
 
                 for idx_temp in range(args.fw_epochs):
@@ -352,31 +338,15 @@ def train_model(
                         + lambda_entropy * entropy_loss_value
                         + reg_weight * reg_loss
                     )
-                    logits_test, _, _ = log(
-                        eivec,
-                        eival,
-                        reg_thres,
-                        features,
-                        sp_adj,
-                        sparse,
-                        model.gcn,
-                        idx_test,
-                        test_embs,
-                    )
-                    preds = torch.argmax(logits_test, dim=1).cuda()
-                    acc = torch.sum(preds == test_lbls).float() / test_lbls.shape[0]
-
-                    if acc > best_acc:
-                        best_acc = acc
+                    
+                    if loss < best_loss:
+                        best_loss = loss
                         cnt_wait = 0
-
                     else:
                         cnt_wait += 1
 
                     if cnt_wait == patience:
-                        print(
-                            f"Early stopping at iteration {idx_temp + 1} with best accuracy: {best_acc:.4f}"
-                        )
+                        print(f"Early stopping at iteration {idx_temp + 1} with best loss: {best_loss:.4f}")
                         break
 
                     loss.backward(retain_graph=True)
